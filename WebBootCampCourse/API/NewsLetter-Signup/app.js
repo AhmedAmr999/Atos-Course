f0bb6553af7483ef6beb9518e652cac8d3ef857c
Ahmed Amr <67699360+AhmedAmr999@users.noreply.github.com>
2023-04-19 13:13:44 +0200

    heruko_commit

--- WebBootCampCourse/API/NewsLetter-Signup/app.js
+++ WebBootCampCourse/API/NewsLetter-Signup/app.js
@@ -3,44 +3,62 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const https=require("https");
+const https = require("https");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/signup.html");
 });
+
 app.post("/", (req, res) => {
-  //console.log(req.body.firstName);
-  //console.log(req.body.lastName);
-  //console.log(req.body.email);
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
 
   const data = {
-    members: {
-      email_address: email,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName,
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
       },
-    },
+    ],
   };
   const jsonData = JSON.stringify(data);
   const url = "https://us17.api.mailchimp.com/3.0/lists/30776e16c0";
-  const options={
-    method:"POST",
-    
-  }
-  https.request(url,options,(response)=>{
-
-  })
-
+  const options = {
+    method: "POST",
+    auth: "maso9:4a1c15b20810abcf6a138bf143fd22c-us17",
+  };
+  const request = https.request(url, options, (response) => {
+    if (response.statusCode === 200) {
+      res.sendFile(
+        __dirname + "/success.html"
+      );
+    } else {
+      res.sendFile(
+        __dirname + "/failure.html"
+      );
+    }
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
-app.listen(3000, () => {
+
+app.post("/failure",(req,res)=>{
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT||3000, () => {
   console.log("Port 3000 is running");
 });
 
